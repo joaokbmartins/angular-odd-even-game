697c92d5770c2341444675c2d0a0aa0634a7125c
Joao Martins <martins.joao1909@gmail.com>
2020-11-22 08:56:24 -0300

    Adding game components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './components/game-control/game-control.component';
+import { OddComponent } from './components/odd/odd.component';
+import { EvenComponent } from './components/even/even.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EvenComponent,
+    OddComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/game-control/game-control.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html'
+})
+
+export class GameControlComponent {
+  
+}
